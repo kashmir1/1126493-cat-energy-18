93ca67af32dc2982181d323d2345f590ab592040
Alex <lzlin@mail.ru>
2020-01-22 23:53:03 +1000

    исправляет ошибки по  БЭМ

--- source/js/menu.js
+++ source/js/menu.js
@@ -4,11 +4,11 @@ var navToggle = document.querySelector('.main-nav__toggle');
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (!navToggle.classList.contains('main-nav--closed')) {
-    navToggle.classList.add('main-nav--closed');
-    navToggle.classList.remove('main-nav--opened');
+  if (!navToggle.classList.contains('main-nav__toggle--closed')) {
+    navToggle.classList.add('main-nav__toggle--closed');
+    navToggle.classList.remove('main-nav__toggle--opened');
   } else {
-    navToggle.classList.remove('main-nav--closed');
-    navToggle.classList.add('main-nav--opened');
+    navToggle.classList.remove('main-nav__toggle--closed');
+    navToggle.classList.add('main-nav__toggle--opened');
   }
 })
